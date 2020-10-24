04e8c4a06b210a709caeff0823cc57c13d545592
Camilo318 <camiloa0318@gmail.com>
2020-10-24 14:36:56 -0500

    Add new layout

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -14,40 +14,43 @@ const Note = (props) => {
             <div className="note__body">
                 <div className="note__title">
                     {title}
+                    <div className="note__options">
+                        <Link
+                        className='note__edit'
+                        to={{
+                            pathname:'/edit',
+                                state: {
+                                    message: 'Edit Note',
+                                    method: 'PUT',
+                                    title,
+                                    content,
+                                    category,
+                                    id: `/${_id}`
+                                }
+                            }}>
+                            Edit
+                        </Link>
+                        <Link
+                        className='note__delete'
+                        to={{
+                                pathname:'/delete',
+                                state: {
+                                    id: _id
+                                }
+                            }}>
+                            Delete
+                        </Link>
+                    </div>
                 </div>
+
                 <div className="note__category">
                     {category}
                 </div>
+
                 <div className="note__content">
                     {content}
                 </div>
-                <div className="note__options">
-                    <Link
-                    className='note__edit'
-                    to={{
-                        pathname:'/edit',
-                            state: {
-                                message: 'Edit Note',
-                                method: 'PUT',
-                                title,
-                                content,
-                                category,
-                                id: `/${_id}`
-                            }
-                        }}>
-                        Edit
-                    </Link>
-                    <Link
-                    className='note__delete'
-                    to={{
-                            pathname:'/delete',
-                            state: {
-                                id: _id
-                            }
-                        }}>
-                        Delete
-                    </Link>
-                </div>
+                
                 <div className="note__timestamps">
                     <p>Updated {format(updatedAt)}</p>
                     <p>Created {format(createdAt)}</p>
