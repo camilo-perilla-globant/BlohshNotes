24f4254d939c4efa20c06d84b9aca7b795cd1e25
Camilo318 <camiloa0318@gmail.com>
2020-10-26 20:03:37 -0500

    Add sign-in

--- server/api/users.js
+++ server/api/users.js
@@ -1,11 +1,64 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
-router.post('/sign-in', async (req, res) => {
-    
+//basic 
+require('../auth/basic')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.find({})
+        if (!users) return next()
+        res.json({
+            info: users
+        })
+    } catch (error) {
+        next(error)
+    }
 })
 
+
+router.post('/sign-in', async (req, res, next) => {
+    passport.authenticate('basic', (error, user) => {
+        try {
+            if (error || !user) {
+                next(error)
+            }
+
+            req.login(user, { session: false }, async (err) => {
+                if (err) return next(err)
+
+                const { _id: id, username, email } = user
+                const payload = {
+                    sub: id,
+                    username,
+                    email
+                }
+
+                const token = jwt.sign(payload, process.env.SECRET, {
+                    expiresIn: '20m'
+                })
+
+                return res.status(200).json({
+                    token,
+                    user: {
+                        id,
+                        username,
+                        email
+                    }
+                })
+
+            })
+        } catch (error) {
+            next(error)
+        }
+    })(req, res, next)
+})
+
+
+//Create user
 router.post('/', async (req, res, next) => {
     const errors = []
     const { username, email, password, confirm_password } = req.body
