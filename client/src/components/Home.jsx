5b5237b35f57082991ba1ef54e2caf7cab67b53d
Camilo318 <camiloa0318@gmail.com>
2020-11-17 19:26:53 -0500

    Add images Add features items

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,11 @@ import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+import notes from '../assets/images/notes.svg'
+import secure from '../assets/images/secure.svg'
+import responsive from '../assets/images/responsive-design.svg'
+import sort from '../assets/images/sort.svg'
+
 gsap.registerPlugin(TextPlugin, ScrollTrigger);
 
 const Home = () => {
@@ -33,7 +38,7 @@ const Home = () => {
             text: ' secure',
             scrollTrigger: {
                 trigger: '.secure-1',
-                start: 'top+=20px center',
+                start: 'top+=15px center',
                 toggleActions: 'restart none none reverse',
                 scrub: 0.5,
                 // markers: true
@@ -44,14 +49,22 @@ const Home = () => {
             text: ' safe',
             scrollTrigger: {
                 trigger: '.secure-2',
-                start: 'top center',
+                start: 'top+=15px center',
                 toggleActions: 'restart none none reverse',
                 scrub: 0.5,
                 // markers: true
             }
         })
     }, [])
-
+    const FeatureItem = ({icon, children}) => {
+        return (
+            <div className="home__item">
+                <img src={icon} alt="Feature icon"/>
+                { children[0] }
+                { children[1] }
+            </div>
+        )
+    }
     return (
         <div className='home'>
             <h1>
@@ -60,7 +73,7 @@ const Home = () => {
             <h5>
                 Blohsh Notes allows you to capture those ideas that came out of nowhere and find them fast
             </h5>
-            <div className="note-demo">
+            <div className="home__demo">
                 <p className="feature" ref={addToArr}>Create notes</p>
                 <p className="feature" ref={addToArr}>Edit notes</p>
                 <p className="feature" ref={addToArr}>Delete notes</p>
@@ -68,11 +81,34 @@ const Home = () => {
                 <p className="feature" ref={addToArr}>Sort notes</p>
                 <p className="feature" ref={addToArr}>Your notes</p>
                 <p className="feature" ref={addToArr}>Your info</p>
-                <p className="feature" ref={addToArr}>Keep it
+                <p className="feature secret" ref={addToArr}>Keep it
                 <span className='secure-1'> ••••••</span></p>
                 <p className="feature" ref={addToArr}>Keep it
                 <span className='secure-2'> ••••</span></p>
 
+            </div>
+
+            <div className="home__container">
+                <h2>Sneak peek</h2>
+                <FeatureItem icon={notes}>
+                    <h3>Your notes, a piece of cake</h3>
+                    <p>Keeping important info has never been easier</p>
+                </FeatureItem>
+
+                <FeatureItem icon={secure}>
+                    <h3>Privacy, a big deal</h3>
+                    <p>All data is encrypted before it goes up to the server</p>
+                </FeatureItem>
+
+                <FeatureItem icon={responsive}>
+                    <h3>Notes, notes anywhere</h3>
+                    <p>Keep important info handy. Manage notes from your phone, tablet or desktop</p>
+                </FeatureItem>
+
+                <FeatureItem  icon={sort}>
+                    <h3>Find things fast</h3>
+                    <p>Bunch of options to search and sort notes, so you get what you need when you need it</p>
+                </FeatureItem>
             </div>
         </div>
     )
